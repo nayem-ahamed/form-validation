7936470977d0e07d1a41f8fddf0da28de229fe66
nayem-ahamed <nayemahamed0717@gmail.com>
2024-02-23 19:10:38 +0530

    first commit

--- index.js
+++ index.js
@@ -1,122 +1,117 @@
 let formData = document.querySelector(".form");
-let submitButton = document.querySelector(".button");
-let errorMessages = document.querySelectorAll(".error-message");
-let emptyFieldMessage = document.querySelectorAll(".empty-field");
-let showPasswordBtn = document.querySelector(".btn");
-let fnFlag, lnFlag, eFlag, pFlag;
-let firstname, lastname, email, password;
-let fnTarget, lnTarget, eTarget, pTarget;
-let field;
+let submitBtn = document.querySelector(".button");
+let errMsg = document.querySelectorAll(".error-message")
+let emtField = document.querySelectorAll(".empty-field")
+let showPwdBtn = document.querySelector(".btn")
+let firstName, lastName, email, password
+let fnTarget, lnTarget, emailTarget, pwdTarget
+let fnFlag, lnFlag, emailFlag, pwdFlag
+let field
 
 let nameRegex = /^[a-z]+$/i;
 let emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-let pwdRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+let passRegex = /^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,}$/;
 
-for (let errorMessage of errorMessages) {
-  errorMessage.classList.add("d-none");
+
+for (let err of errMsg) {
+    err.classList.add("d-none")
+}
+for (let emt of emtField) {
+    emt.classList.add("d-none")
 }
 
-for (let message of emptyFieldMessage) {
-  message.classList.add("d-none");
-}
 
 formData.addEventListener("keyup", (e) => {
-  e.preventDefault();
-  field = e.target.dataset.key;
-  switch (field) {
-    case "firstName":
-      firstname = e.target.value;
-      fnTarget = e.target;
-      break;
-    case "lastName":
-      lastname = e.target.value;
-      lnTarget = e.target;
-      break;
-    case "email":
-      email = e.target.value;
-      eTarget = e.target;
-      break;
-    case "password":
-      password = e.target.value;
-      pTarget = e.target;
-      break;
-    default:
-      firstname = lastname = email = password = "";
-      break;
-  }
+    e.preventDefault();
+    field = e.target.dataset.key;
+    switch (field) {
+        case "firstName":
+            firstName = e.target.value;
+            fnTarget = e.target;
+            break;
+        case "lastName":
+            lastName = e.target.value;
+            lnTarget = e.target;
+            break;
+        case "email":
+            email = e.target.value;
+            emailTarget = e.target;
+            break;
+        case "password":
+            password = e.target.value;
+            pwdTarget = e.target;
+            break;
+    }
+
+})
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password)
+
+    if (firstName) {
+        if (!nameRegex.test(firstName)) {
+            fnTarget.classList.add("error")
+            errMsg[0].classList.remove("d-none")
+            fnFlag = false
+        } else {
+            fnTarget.classList.remove("error")
+            errMsg[0].classList.add("d-none")
+            fnFlag = true
+        }
+    } else {
+        emtField[0].classList.remove("d-none")
+    }
+    if (lastName) {
+        if (!nameRegex.test(lastName)) {
+            lnTarget.classList.add("error")
+            errMsg[1].classList.remove("d-none")
+            lnFlag = false
+        } else {
+            lnTarget.classList.remove("error")
+            errMsg[1].classList.add("d-none")
+            lnFlag = true
+        }
+    } else {
+        emtField[1].classList.remove("d-none")
+    }
+    if (email) {
+        if (!emailRegex.test(email)) {
+            emailTarget.classList.add("error")
+            errMsg[2].classList.remove("d-none")
+            emailFlag = false
+        } else {
+            emailTarget.classList.remove("error")
+            errMsg[2].classList.add("d-none")
+            emailFlag = true
+        }
+    } else {
+        emtField[2].classList.remove("d-none")
+    }
+    if (password) {
+        if (!passRegex.test(password)) {
+            pwdTarget.classList.add("error")
+            errMsg[3].classList.remove("d-none")
+            pwdFlag = flase
+        } else {
+            pwdTarget.classList.remove("error")
+            errMsg[3].classList.add("d-none")
+            pwdFlag = true
+        }
+    } else {
+        emtField[3].classList.remove("d-none")
+    }
+    if (fnFlag && lnFlag && emailFlag && pwdFlag) {
+        window.location.href = "/success.html"
+    }
+
 });
 
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(fnTarget, lnTarget, eTarget, pTarget);
-  if (firstname) {
-    emptyFieldMessage[0].classList.add("d-none");
-    if (!nameRegex.test(firstname)) {
-      fnTarget.classList.add("error");
-      errorMessages[0].classList.remove("d-none");
-      fnFlag = false;
+showPwdBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    if (pwdTarget.getAttribute("type") === "text") {
+        pwdTarget.setAttribute("type", "password")
     } else {
-      errorMessages[0].classList.add("d-none");
-      fnTarget.classList.remove("error");
-      fnFlag = true;
+        pwdTarget.setAttribute("type", "text")
     }
-  } else {
-    //Show empty field message
-    emptyFieldMessage[0].classList.remove("d-none");
-  }
-  if (lastname) {
-    emptyFieldMessage[1].classList.add("d-none");
-    if (!nameRegex.test(lastname)) {
-      lnTarget.classList.add("error");
-      errorMessages[1].classList.remove("d-none");
-      lnFlag = false;
-    } else {
-      errorMessages[1].classList.add("d-none");
-      lnTarget.classList.remove("error");
-      lnFlag = true;
-    }
-  } else {
-    emptyFieldMessage[1].classList.remove("d-none");
-  }
-  if (email) {
-    emptyFieldMessage[2].classList.add("d-none");
-    if (!emailRegex.test(email)) {
-      eTarget.classList.add("error");
-      errorMessages[2].classList.remove("d-none");
-      eFlag = false;
-    } else {
-      errorMessages[2].classList.add("d-none");
-      eTarget.classList.remove("error");
-      eFlag = true;
-    }
-  } else {
-    emptyFieldMessage[2].classList.remove("d-none");
-  }
-  if (password) {
-    emptyFieldMessage[3].classList.add("d-none");
-    if (!pwdRegex.test(password)) {
-      pTarget.classList.add("error");
-      errorMessages[3].classList.remove("d-none");
-      pFlag = false;
-    } else {
-      errorMessages[3].classList.add("d-none");
-      pTarget.classList.remove("error");
-      pFlag = true;
-    }
-  } else {
-    emptyFieldMessage[3].classList.remove("d-none");
-  }
-  if (fnFlag && lnFlag && eFlag && pFlag) {
-    fnTarget.value = lnTarget.value = eTarget.value = pTarget.value = "";
-    window.location.href = "success.html";
-  }
-});
-
-showPasswordBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  if (pTarget.getAttribute("type") === "text") {
-    pTarget.setAttribute("type", "password");
-  } else {
-    pTarget.setAttribute("type", "text");
-  }
-});
+})
